f8fabc0ebc20ef9096b0fd81f84e1a8d44dad475
Narek Kerobian <narek.k@bracketnco.com>
2022-07-21 20:14:44 +0400

    Add categories and subcategories when scraping

--- model/page.go
+++ model/page.go
@@ -6,6 +6,8 @@ type Page struct {
     gorm.Model
     Parent      uint        `gorm:"references:ID"`
     Title       string      `gorm:"title"` 
+    Category    string      `gorm:"category"` 
+    Subcategory string      `gorm:"subcategory"` 
     Text        Texts       `gorm:"serializer:json"`
     UrlOriginal string      `gorm:"url"`
 }
--- service/scrape.go
+++ service/scrape.go
@@ -5,8 +5,10 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"gitlab.com/bracketnco/odoo-scraper/model"
+	"gorm.io/gorm"
 )
 
 // Prompts for inputs and runs the scraper
@@ -67,69 +69,19 @@ func Scrape(dbPath string, url string, parentId uint) {
     // Declare new collector
     c := colly.NewCollector(
         colly.MaxDepth(1),
-        colly.Async(false),
+        colly.Async(true),
     )
 
     // Set parallelism to 1
     c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 1})
 
-    // On every a element which has href attribute call callback
-	c.OnHTML(
-        ".toctree-l1.o_menu_applications_inventory_and_mrp_inventory_management a[href].reference.internal", 
-        func(e *colly.HTMLElement) {
-            link := e.Attr("href")
-
-            // Print link
-            if link != "#" {
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-                // Visit link found on page
-                // Only those links are visited which are in AllowedDomains
-                c.Visit(e.Request.AbsoluteURL(link))
-
-                // Scrape the main body
-                c.OnHTML("article#o_content", func(e *colly.HTMLElement) {
-                    // Get title
-                    titleNode := e.DOM.Find("h1")
-
-                    if titleNode.Length() > 0 {
-                        var page *model.Page
-                        page = new(model.Page)
-
-                        // Get the title
-                        page.Title = strings.Trim(titleNode.First().Text(), "¶")
-                        
-                        // Remove the parent node
-                        titleNode.First().Remove()
-
-                        pageText := ""
-
-                        // Scrape the content
-                        e.ForEach("section", func(i int, h *colly.HTMLElement) {
-                            html, err := h.DOM.Html()
-                            if err != nil {
-                                panic(err)
-                            }
-
-                            pageText = pageText + strings.TrimSpace(html)
-                            page.UrlOriginal = link
-                        })
-
-                        // Fix image urls
-                        r, _ := regexp.Compile("(?:[\"])((../)+)")
-                        text := model.Localized{
-                            Lang: "en",
-                            Text: r.ReplaceAllString(pageText, "\"https://www.odoo.com/documentation/15.0/"),
-                        }
-
-                        page.Text = append(page.Text, text)
-                        page.Parent = parentId
-
-                        // Persist page
-                        db.Create(page)
-                    }
-                })
-            }
-	})
+    if parentId == 0 {
+        // Scrape parent page
+        scrapeParent(db, c)
+    } else {
+        // Scrape child page
+        scrapeChild(db, c, parentId)
+    }
 
     // Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
@@ -138,4 +90,154 @@ func Scrape(dbPath string, url string, parentId uint) {
 
     // c.Visit("https://www.odoo.com/documentation/15.0/applications/inventory_and_mrp/inventory.html")
     c.Visit(url)
+
+    c.Wait()
 }
+
+// Scrape parent page
+func scrapeParent(db *gorm.DB, c *colly.Collector) {
+    // On every a element which has href attribute call callback
+	c.OnHTML(
+        "#o_content > div[role=\"main\"] > section:first-child", 
+        func(e *colly.HTMLElement) {
+            var page *model.Page
+            page = new(model.Page)
+
+            // Set parent page title
+            title := e.ChildText("h1")
+            page.Title = strings.Trim(title, "¶")
+
+            // Set parent page original url
+            page.UrlOriginal = e.Request.AbsoluteURL(e.Request.URL.Path)
+
+            // Set parent page texts
+            pageText := ""
+            e.ForEach("section > p", func (i int, el *colly.HTMLElement) {
+                html, err := el.DOM.Html()
+                if err != nil {
+                    panic(err)
+                }
+                pageText = pageText + "<p>" + strings.TrimSpace(html) + "</p>"
+            })
+
+            // Fix image urls
+            r, _ := regexp.Compile("(?:[\"])((../)+)")
+            text := model.Localized{
+                Lang: "en",
+                Text: r.ReplaceAllString(pageText, "\"https://www.odoo.com/documentation/15.0/"),
+            }
+
+            page.Text = append(page.Text, text)
+
+            // Persist page
+            db.Create(page)
+        })
+}
+
+// Scrape child page
+func scrapeChild(db *gorm.DB, c *colly.Collector, parentId uint) {
+    // On every a element which has href attribute call callback
+	c.OnHTML(
+        ".toctree-wrapper .toctree-l1", 
+        func(e *colly.HTMLElement) {
+            
+            category := ""
+
+            // Get category
+            e.ForEach(".toctree-l1 > a", func(i int, elCat *colly.HTMLElement) {
+                category = strings.TrimSpace(elCat.Text) 
+
+                // Get subcategory
+                e.ForEach(".toctree-l1 > ul li.toctree-l2", func(i int, elSubCat *colly.HTMLElement) {
+                    subcategory := ""
+                    elSubCat.ForEach("li.toctree-l2 > a", func(i int, elSubCatTitle *colly.HTMLElement) {
+                        subcategory = strings.TrimSpace(elSubCatTitle.Text)
+                        parent := elSubCatTitle.DOM.Parent()
+
+                        // Define link selector and check if there are subcategories
+                        linkSelector := "li.toctree-l3 a"
+                        if len(parent.Find(linkSelector).Nodes) == 0 {
+                            subcategory = ""
+                            linkSelector = "li.toctree-l2 a"
+                        } 
+
+                        // Loop through links and run the scraper
+                        parent.Find(linkSelector).Each(func(i int, link *goquery.Selection) {
+                            href, _ := link.Attr("href")
+                            requestUrl := e.Request.AbsoluteURL(href)
+
+                            c.Visit(requestUrl)
+
+                            // Scrape page
+                            scrapePage(
+                               db, 
+                               c, 
+                               parentId, 
+                               requestUrl, 
+                               category, 
+                               subcategory, 
+                            )
+                        })
+                    })
+                }) 
+            }) 
+        })
+}
+
+// Scrape documentation pages
+func scrapePage(
+    db *gorm.DB, 
+    c *colly.Collector, 
+    parentId uint,
+    requestUrl string,
+    category string, 
+    subcategory string,
+) {
+    // Scrape the main body
+    c.OnHTML("article#o_content div[role=\"main\"]", func(e *colly.HTMLElement) {
+        if e.Request.URL.String() == requestUrl {
+            // Get title
+            titleNode := e.DOM.Find("h1")
+
+            if titleNode.Length() > 0 {
+                var page *model.Page
+                page = new(model.Page)
+
+                // Get the title
+                page.Title = strings.Trim(titleNode.First().Text(), "¶")
+                
+                // Remove the parent node
+                titleNode.First().Remove()
+
+                pageText := ""
+
+                // Scrape the content
+                e.ForEach("section", func(i int, h *colly.HTMLElement) {
+                    html, err := h.DOM.Html()
+                    if err != nil {
+                        panic(err)
+                    }
+
+                    pageText = pageText + strings.TrimSpace(html)
+                    page.UrlOriginal = requestUrl
+                })
+
+                // Fix image urls
+                r, _ := regexp.Compile("(?:[\"])((../)+)")
+                text := model.Localized{
+                    Lang: "en",
+                    Text: r.ReplaceAllString(pageText, "\"https://www.odoo.com/documentation/15.0/"),
+                }
+
+                page.Text = append(page.Text, text)
+                page.Parent = parentId
+                page.Category = category
+                page.Subcategory = subcategory
+
+                // Persist page
+                db.Create(page)
+            }
+        }
+    })
+}
+
